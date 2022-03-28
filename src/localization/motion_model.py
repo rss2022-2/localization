76525eff429c60f4c27c0d51cf62cbff6226b8cb
Quang Kieu <quang.p.kieu@gmail.com>
2022-03-27 20:24:14 -0400

    Add base motion model

--- src/localization/motion_model.py
+++ src/localization/motion_model.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class MotionModel:
 
     def __init__(self):
@@ -6,10 +8,36 @@ class MotionModel:
         # TODO
         # Do any precomputation for the motion
         # model here.
-
-        pass
+        self.a = [
+                    0,  # ax
+                    0,  # ay
+                    0,  # at
+                 ] 
 
         ####################################
+
+
+    @staticmethod
+    def __sample_normal(val, a):
+        return val - a*val*np.random.standard_normal()
+
+    def __sample_motion_model_odometry(self, position, odometry):
+        x = position[0]
+        y = position[1]
+        t = position[2]
+
+        dx = odometry[0]
+        dy = odometry[1]
+        dt = odometry[2]
+
+        res = [0, 0, 0]
+        abs_dx = dx*np.cos(t) - dy*np.sin(t)
+        abs_dy = dx*np.sin(t) + dy*np.cos(t)
+        res[0] = x + MotionModel.__sample_normal(abs_dx, self.a[0])
+        res[1] = y + MotionModel.__sample_normal(abs_dy, self.a[1])
+        res[2] = t + MotionModel.__sample_normal(dt, self.a[2])
+        return res
+
 
     def evaluate(self, particles, odometry):
         """
@@ -32,7 +60,6 @@ class MotionModel:
         
         ####################################
         # TODO
-
-        raise NotImplementedError
+        return [self.__sample_motion_model_odometry(particle, odometry) for particle in particles] 
 
         ####################################
